5f6d0a84df620d727bdca6423a526c3aa446f8f9
kmean <kim.ranzani@gmail.com>
2025-02-15 09:08:36 +0100

    correct repo name

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ INSTALL_DIR="/usr/local/bin"
 SCRIPT_NAME="header"
 
 # Download the header.sh script from GitHub and save it as 'header'
-curl -o "$INSTALL_DIR/$SCRIPT_NAME" "https://raw.githubusercontent.com/KMean/header-maker/main/bin/header.sh" || {
+curl -o "$INSTALL_DIR/$SCRIPT_NAME" "https://raw.githubusercontent.com/KMean/header-maker/master/bin/header.sh" || {
   echo "Error downloading the script."
   exit 1
 }
